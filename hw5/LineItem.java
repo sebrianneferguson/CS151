fe90e670e31503fcbdd8533f6d8470c5503a2b56
sebrianneferguson <42628590+sebrianneferguson@users.noreply.github.com>
2018-10-09 17:42:12 -0700

    HW5 - Invoice

---
+++ hw5/LineItem.java
@@ -0,0 +1,25 @@
+package hw5;
+
+/**
+ * A line item in an invoice.
+ */
+public interface LineItem {
+	/**
+	 * Gets the price of this line item.
+	 * 
+	 * @return the price
+	 */
+	double getPrice();
+
+	/**
+	 * Gets the description of this line item.
+	 * 
+	 * @return the description
+	 */
+	
+	//added by Sebrianne Ferguson
+	int getQuantity();
+	void increaseQuantity();
+	
+	String toString();
+}
